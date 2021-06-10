fc4ba0f038a6efc0c8d2291f0fb26c7abc9875b1
Francesco Urbani <francescourbanidue@gmail.com>
2021-06-10 18:31:35 +0200

    added resistors correctly

---
+++ README.md
@@ -0,0 +1,5 @@
+Infinite grid of resistors problem ([xkcd.com/356/](https://xkcd.com/356/))
+
+
+[![](https://www.mbeckler.org/resistor_grid/nerd_sniping_s.png)](https://xkcd.com/356/)
+
--- infinite_grid.py
+++ infinite_grid.py
@@ -1,6 +1,4 @@
 """
-* SImple voltage calc
-
 V1 0 1 dc 1V
 R1 1 0 1.0 
 
@@ -17,31 +15,51 @@ print all
 import sys
 
 N = int(sys.argv[1])
+if N % 2 != 0: raise("Give me an even number")
+M = N + 1
 
-out = f"* Infinite grid of resistors, N = {N}\n"
 
+out = f"* Infinite grid of resistors, N = {N}, M = {M}\n"
 
 # put horizontal resistors in place
-for i in range(N*N - 1):
+resistor_number = 0
+
+for i in range(N*M - 1):
+    if (i+1) % M == 0:
+        continue
+    else:
+        out += f"R{resistor_number} {i} {i+1} 1.0\n"
+        resistor_number += 1
+
+# put vertical resistors in place
+for i in range(N*M - 1):
     if (i+1) % N == 0:
         continue
     else:
-        out += f"R{i}_{i+1} {i} {i+1} 1.0\n"
-
-for i in range(N*N - 1):
-    if (i+1) % N == 0:
-        continue
-    else:
-        out += f"R{i}_{i+N} {i} {i+N} 1.0\n"
+        out += f"R{resistor_number} {i} {i+M} 1.0\n"
+        resistor_number += 1
 
 
-out += "\nV1 0 1 dc 1V\n"
+Vx = 11
+Vy = 8
+
+out += f"\nV1 {Vx} {Vy} dc 1V\n"
 
 out += """
 .control
 op
-print all
+print i(v1)
+* print all
 .endc
-"""
+\n"""
+
+# add some commentary at the end of the file
+out += f"* Grid size = {N} * {M}\n"
+out += f"* # resistors = {resistor_number}\n"
+
+
+assert resistor_number == ( (N-1) * M + (M-1) * N )
+
+with open("infinite_grid.cir", "w") as f: f.write(out)
 
 print(out)
